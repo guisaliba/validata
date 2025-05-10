55a3cde96a4c6318c959be64911dff518d3cd2ef
Carlos Ferreira <carlos.ferreira@onfly.com.br>
2025-05-09 21:44:17 -0300

    feat: create entities

---
+++ src/modules/products/entities/product.entity.ts
@@ -0,0 +1,56 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+  OneToOne,
+} from 'typeorm';
+import { Stock } from '../../stocks/entities/stock.entity';
+import { Batch } from '../../batches/entities/batch.entity';
+import { SaleItem } from '../../sales/entities/sale_item.entity';
+import { Discount } from '../../discounts/entities/discount.entity';
+import { SalesAnalysisProduct } from '../../analysis/entities/sales_analysis_product.entity';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  category: string;
+
+  @Column('float')
+  base_price: number;
+
+  @Column('float')
+  sales_frequency: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @OneToMany(() => Stock, (stock) => stock.product)
+  stocks: Stock[];
+
+  @OneToMany(() => Batch, (batch) => batch.product)
+  batches: Batch[];
+
+  @OneToMany(() => SaleItem, (saleItem) => saleItem.product)
+  saleItems: SaleItem[];
+
+  @OneToOne(() => Discount, (discount) => discount.product)
+  discount: Discount;
+
+  @OneToMany(
+    () => SalesAnalysisProduct,
+    (analysisProduct) => analysisProduct.product,
+  )
+  analysisProducts: SalesAnalysisProduct[];
+}
