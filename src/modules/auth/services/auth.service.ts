89992c6149ecd72f3e2dd94ee5b4aebfc4297d1b
Carlos Ferreira <carlos.ferreira@onfly.com.br>
2025-05-31 00:22:32 -0300

    feat: implement authentication module with login and registration functionality

---
+++ src/modules/auth/services/auth.service.ts
@@ -0,0 +1,59 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../../users/services/users.service';
+import {
+  IAuthService,
+  IAuthPayload,
+  IAuthResponse,
+} from '../interfaces/auth.interface';
+import * as bcrypt from 'bcrypt';
+import { IUser } from 'src/modules/users/interfaces/user.interface';
+
+@Injectable()
+export class AuthService implements IAuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.usersService.findByEmail(email);
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return user;
+    }
+
+    return null;
+  }
+
+  async login(email: string, password: string): Promise<IAuthResponse> {
+    const user = await this.validateUser(email, password);
+    if (!user) {
+      throw new UnauthorizedException('Credenciais inválidas');
+    }
+
+    const payload: IAuthPayload = { sub: user.id, email: user.email };
+    return {
+      access_token: this.generateToken(payload),
+      user,
+    };
+  }
+
+  async register(
+    userData: Omit<IUser, 'id' | 'created_at' | 'updated_at'>,
+  ): Promise<Omit<IUser, 'password'>> {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(userData.password, salt);
+
+    const user = await this.usersService.create({
+      ...userData,
+      password: hashedPassword,
+    });
+
+    return user;
+  }
+
+  generateToken(payload: IAuthPayload): string {
+    return this.jwtService.sign(payload);
+  }
+}
