fa73e5b960def0f6ebf828826db09b48bee1be47
Carlos Ferreira <carlos.ferreira@onfly.com.br>
2025-06-01 18:30:58 -0300

    feat: enable CORS in the main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ClassSerializerInterceptor } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-
+  app.enableCors();
   await app.listen(process.env.PORT ?? 8000);
 }
 
